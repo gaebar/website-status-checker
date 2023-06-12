1f777f171a0e499c40a9bc3870e3857cb3900c44
Gaetano Barreca <gaetanobarreca@gmail.com>
2023-06-12 16:29:50 -0700

    first commit

---
+++ src/main/java/com/example/WebsiteChecker.java
@@ -0,0 +1,31 @@
+package com.example;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class WebsiteChecker {
+    public static void main(String[] args) {
+        while (true) {
+            try {
+                URL url = new URL("https://leetcode.com/explore/interview/card/facebook/");
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.setRequestMethod("GET");
+                int responseCode = connection.getResponseCode();
+                if (responseCode == 200) {
+                    System.out.println("Leetcode website is back online!");
+                    break;
+                } else {
+                    System.out.println("Leetcode website seems down... waiting for it to come back online.");
+                }
+            } catch (Exception e) {
+                System.out.println("Error: " + e.getMessage());
+            }
+
+            try {
+                Thread.sleep(60000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}

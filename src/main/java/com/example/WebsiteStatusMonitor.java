015114f0e4b50191cd6924dc7e64b5d3cc7bf002
Gaetano Barreca <gaetanobarreca@gmail.com>
2023-06-15 11:21:59 -0700

    creating .env

--- src/main/java/com/example/WebsiteStatusMonitor.java
+++ src/main/java/com/example/WebsiteStatusMonitor.java
@@ -2,6 +2,10 @@ package com.example;
 
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Properties;
+
+import javax.mail.*;
+import javax.mail.internet.*;
 
 public class WebsiteStatusMonitor {
     public static void main(String[] args) {
@@ -13,6 +17,7 @@ public class WebsiteStatusMonitor {
                 int responseCode = connection.getResponseCode();
                 if (responseCode == 200) {
                     System.out.println("Leetcode website is back online!");
+                    sendEmail(); // Trigger email notification when website is back online
                     break;
                 } else {
                     System.out.println("Leetcode website seems down... waiting for it to come back online.");
@@ -22,10 +27,43 @@ public class WebsiteStatusMonitor {
             }
 
             try {
-                Thread.sleep(60000);
+                Thread.sleep(60000); // Wait for 60 seconds before checking the website status again
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
+        }
+    }
+
+    /**
+     * Sends an email notification using JavaMail API and Amazon SES.
+     */
+    private static void sendEmail() {
+        String recipientEmail = "email@example.com";
+        String senderEmail = "ses.gaetanobarreca.dev";
+        String awsAccessKeyId = System.getenv("AWS_ACCESS_KEY_ID");
+        String awsSecretAccessKey = System.getenv("AWS_SECRET_ACCESS_KEY");
+        String awsRegion = "your-aws-region"; // Replace with your AWS region
+
+        Properties properties = new Properties();
+        properties.put("mail.transport.protocol", "aws");
+        properties.put("mail.aws.user", awsAccessKeyId);
+        properties.put("mail.aws.password", awsSecretAccessKey);
+        properties.put("mail.aws.region", awsRegion);
+
+        Session session = Session.getInstance(properties);
+
+        try {
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(senderEmail));
+            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(recipientEmail));
+            message.setSubject("Website Status Alert");
+            message.setText("The website is back online!");
+
+            Transport.send(message); // Send the email using JavaMail API and Amazon SES
+
+            System.out.println("Email sent successfully.");
+        } catch (Exception e) {
+            System.out.println("Error sending email: " + e.getMessage());
         }
     }
 }
